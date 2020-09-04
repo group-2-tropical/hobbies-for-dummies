d6e8e3e8eb3f620922b6e77814495425fdeb2fa9
Eas <me@eas.web.id>
2020-09-04 10:47:38 +0700

    err handling improvements, change to async-await

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,81 +1,78 @@
 const { User } = require('../models')
 const { generateToken } = require('../helpers/jwt')
-const { comparePassword } = require('../helpers/bcrypt')
+const { comparePassword, createHash, generateRandomPassword } = require('../helpers/bcrypt')
 
 class UserController {
-    static async register (req, res) {
-        const { email, password } = req.body
+    static async register(req, res, next) {
         try {
-            const user = await User.create({email, password})
-            return res.status(201).json({email})
+            const userData = {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password
+            }
+            console.log(userData);
+            const user = await User.create(userData)
+
+            const { id, username, email } = user
+
+            return res.status(201).json({ id, username, email })
         }
-        catch(err) {
-            return res.status(500).json({message: "Internal server error"})
+        catch (err) {
+            return next(err)
         }
     }
-    static async login (req, res) {
-        const {email, password} = req.body
+
+    static async login(req, res, next) {
         try {
-            const user = await User.findOne({where: { email }})
-            if (!user) {
-                return res.status(400).json({message: "Invalid Email"})
-                // throw {message: "Invalid Email", statusCode: 400}
-            }
-            const isValid = comparePassword(password, user.password)
-            if(isValid) {
-                const access_token = generateToken({email: user.email, id: user.id})
-                return res.status(200).json({access_token})
+            const { username, password } = req.body
+            const user = await User.findOne({ where: { username } })
+            const isValid = comparePassword(password, user.password || '')
+
+            if (user && isValid) {
+                const access_token = generateToken({ email: user.email, id: user.id })
+
+                return res.status(200).json({ access_token })
             } else {
-                return res.status(400).json({message: "Invalid Password"})
-                // throw {message: "Invalid Password", statusCode: 400}
+                throw { code: 401, message: `Oops! Wrong username or password!` }
             }
-        } catch(err) {
-            return res.status(500).json({message: err.message})
-            // return next(err)
+        } catch (err) {
+            return next(err)
         }
     }
 
     // static untuk google login
-    // TODO: bikin async-await
-    static googleLogin (req, res, next) {
-        const client = new OAuth2Client(process.env.CLIENT_ID)
-        const { google_token } = req.headers
-        const data = {} 
+    static async googleLogin(req, res, next) {
+        try {
+            const client = new OAuth2Client(process.env.CLIENT_ID)
+            const { google_token } = req.headers
 
-        client.verifyIdToken({
+            const payload = await client.verifyIdToken({
                 idToken: google_token,
                 audience: process.env.CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
                 // Or, if multiple clients access the backend:
                 //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-        })
-        .then(ticket => {
-            const payload = ticket.getPayload()
-            return payload
-        })
-        .then(payload => {
-            
-            data.username = payload.name
-            data.email = payload.email
-            data.password = 'random123'
-            
-            return User.findOne({where : { email : data.email}})
-        })
-        .then ( user => {
-            if(!user) {
-                return User.create(data)
-            } else {
-                return user
+            })
+                .getPayload()
+
+            const data = {
+                username: payload.name,
+                email: payload.email,
+                password: createHash(generateRandomPassword(15))
             }
-        })
-        .then ( user => {
-            const access_token = generateToken(user)
-            console.log(access_token)
-            return res.status(200).json({access_token})
-        })
-        .catch( err => {
-            console.log(err)
-            next(err)
-        })
+
+            let user = User.findOne({ where: { email: data.email } })
+
+            if (!user) {
+                user = User.create(data)
+            } else {
+                const access_token = generateToken(user)
+
+                return res.status(200).json({ access_token })
+            }
+        }
+        catch (err) {
+            return next(err)
+        }
     }
 }
 
