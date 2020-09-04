01d8b1a17829f655b57c020fa2b98b1d62bd6f8d
Eas <me@eas.web.id>
2020-09-04 10:27:17 +0700

    line 12 fixes, cors should be invoked

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 app
 .use(express.json())
 .use(express.urlencoded({ extended: true }))
-.use(cors)
+.use(cors())
 .use(routes)
 .use(errHandler)
 .listen(port, () => {
