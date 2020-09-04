0ec3bf4cbe165374f1a638bcfb24093b513eeca0
Eas <me@eas.web.id>
2020-09-04 10:48:40 +0700

    API rename

--- server/routes/endpoints/api.js
+++ server/routes/endpoints/api.js
@@ -1,6 +1,6 @@
 const apiEndpoints = require('express').Router()
-const PixabayController = require('../../controllers/PixabyController')
-const JikanController = require('../../controllers/JikanController')
+const PixabayController = require('../../controllers/ApiPixabayController')
+const JikanController = require('../../controllers/ApiJikanController')
 
 apiEndpoints.get('/pixabay', PixabayController.getPixabay)
 apiEndpoints.get('/jikan', JikanController.getJikan)
